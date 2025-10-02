911332bc52ad5e3ba5a938c9ea3c90003de46786
dylandrozario <drozari@bc.edu>
2025-10-02 09:12:55 -0400

    Add getComputerChoice function
    This function gets a random integer from 0-2
    and then returns back either rock, paper, or
    scissors based on which integer it choosen

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+
+function getComputerChoice(){
+    let choice = Math.floor((Math.random() * 3));
+    switch(choice){
+        case 0:
+            return "paper";
+            break;
+        case 1:
+            return "scissor";
+            break;
+        case 2:
+            return "rock";
+            break;
+        default:
+            prompt("Computer Choice Failed");
+    }
+}
