233e8c6ebceb18ccf2c0cd00e83ad9dfd2100c61
dylandrozario <drozari@bc.edu>
2025-10-02 22:05:09 -0400

    Add getHumanChoice function
    getHumanChoice prompts the user for what choice
    they want to perform by entering an integer
    0-2 repreneting a action and returns the english
    equivalent

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     let choice = Math.floor((Math.random() * 3));
+    switch(choice){
+        case 0:
+            return "paper";
+            break;
+        case 1:
+            return "scissor";
+            break;
+        case 2:
+            return "rock";
+            break;
+        default:
+            prompt("Computer Choice Failed");
+    }
+}
+
+function getHumanChoice(){
+    let choice = parseInt(prompt("Enter (0) for paper, (2) for scissors, (3) for rock"));
     switch(choice){
         case 0:
             return "paper";
